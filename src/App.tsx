d7c6057b3574aa9753a3cb3f63293e7d7948ad30
Tomasz <tomaszwojciechowski244@gmail>
2024-08-23 23:30:57 +0200

    add start and stop functionallity

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {useState, useRef} from "react";
 import './App.css'
 import {Title} from "./components/Title/Title.tsx";
 import {Counter} from "./components/Counter/Counter.tsx";
@@ -5,20 +6,43 @@ import {ActionButton} from "./components/ActionButton/ActionButton.tsx";
 import {Table} from "./components/Table/Table.tsx";
 
 export const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef<number | null>(null);
 
   const action = () => {
     console.log("Button clicked");
   }
 
+  const startAction = () => {
+    setIsRunning(true);
+    if (intervalRef.current !== null) {
+      return;
+    }
+    intervalRef.current = setInterval(() => {
+      setCounter(prevState => prevState + 100);
+    }, 100);
+  }
+
+  const stopAction = () => {
+    if (intervalRef.current === null) {
+      return;
+    }
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setIsRunning(false);
+  }
+
+  const showBasicButton = isRunning ? <ActionButton actionName={'stop'} action={stopAction}/> :
+    <ActionButton actionName={'start'} action={startAction}/>;
   return (
     <div className="wrapper">
       <Title/>
 
-      <Counter time={0}/>
+      <Counter time={counter}/>
 
       <div>
-        <ActionButton actionName={'start'} action={action}/>
-        <ActionButton actionName={'stop'} action={action}/>
+        {showBasicButton}
         <ActionButton actionName={'reset'} action={action}/>
         <ActionButton actionName={'lap'} action={action}/>
       </div>
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -5,15 +5,17 @@ type Props = {
 }
 
 export const Counter = ({time}: Props) => {
-  const hours = `${Math.floor(time / 3600)}`
-  const minutes = Math.floor((time % 360000) / 6000);
-  const seconds = Math.floor((time % 6000) / 100);
-  const milliseconds = time % 100;
+  const hours = Math.floor(time / 3600000);
+  const minutes = Math.floor((time % 3600000) / 60000);
+  const seconds = Math.floor((time % 60000) / 1000);
+  const milliseconds = ((time % 1000) / 100);
 
   return (
     <div className="counter">
-      {hours}:{minutes.toString().padStart(2, "0")}:
-      {seconds.toString().padStart(2, "0")}:{milliseconds}
+      {hours.toString().padStart(2, "0")}:
+      {minutes.toString().padStart(2, "0")}:
+      {seconds.toString().padStart(2, "0")}:
+      {milliseconds}
     </div>
-  )
-}
+  );
+};
