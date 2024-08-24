4dea7738475ce46ef580013f825e55918c3db960
Tomasz <tomaszwojciechowski244@gmail>
2024-08-24 21:03:10 +0200

    format time in table record

--- src/components/Table/TableRecord/TableRecord.tsx
+++ src/components/Table/TableRecord/TableRecord.tsx
@@ -6,10 +6,24 @@ type Props = {
 }
 
 export const TableRecord = ({lap, index}: Props) => {
+  const formatTime = () => {
+    const hours = Math.floor(lap / 3600000);
+    const minutes = Math.floor((lap % 3600000) / 60000);
+    const seconds = Math.floor((lap % 60000) / 1000);
+    const milliseconds = ((lap % 1000) / 100);
+
+    return `
+    ${hours < 10 ? '0' + hours : hours}:
+    ${minutes < 10 ? '0' + minutes : minutes}:
+    ${seconds < 10 ? '0' + seconds : seconds}:
+    ${milliseconds}
+    `;
+  }
+
   return (
     <tr>
       <td className="number-cell">{index + 1}</td>
-      <td className="lap-cell">{lap}</td>
+      <td className="lap-cell">{formatTime()}</td>
     </tr>
   )
 }
